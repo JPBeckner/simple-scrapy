fae61feba9a27fd5925417585464c3d39a35128d
JPBeckner <pedrobeckner@hotmail.com>
2021-03-17 00:46:26 -0300

    feat: att runner

--- runner.py
+++ runner.py
@@ -1,11 +1,12 @@
 from scrapy.crawler import CrawlerProcess
 
-from simple_scrapy.spiders import BooksSpider
+from simple_scrapy.spiders import *
 
 
 def run():
     crawler = CrawlerProcess()
     crawler.crawl(BooksSpider)
+    crawler.crawl(QuotesSpider)
     crawler.start()
 
 if __name__ == "__main__":
